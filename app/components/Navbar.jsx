64fc6b6bd37098bfdae9a5463b254d1c250b6812
unknown <yugankbansal.00001276033.aesl@gmail.com>
2023-09-16 14:08:19 +0530

    new commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,11 +4,11 @@ import React, { useState } from "react";
 // import { AiOutlineAlignRight } from 'react-icons/ai';
 import Link from "next/link";
 export default function Navbar() {
-  // const [Toggle,setToggle]=useState(false);
+  const [Toggle,setToggle]=useState(true);
 
-  // const handleClick = () => {
-  //   setToggle(!Toggle);
-  // };
+  const handleClick = () => {
+    setToggle(!Toggle);
+  };
 
   return (
     <div>
@@ -16,41 +16,32 @@ export default function Navbar() {
         <div></div>
        <Link href="/"> <img src="/logo.png" alt="logo" className="logo" /></Link>
         <div className="items" id="features">
+
           <ul className="first hidden md:flex">
             <li><Link href="/">Featured Events</Link></li>
             <li><Link href="/">Inventory</Link></li>
             <li><Link href="/">Docs</Link></li>
           </ul>
-          {/* <ul className="first md:hidden h-screen w-full fixed left-[0] top-[-80px]" >
+
+          <ul className="first md:hidden h-screen w-full fixed left-[0] top-[-80px]" >
             <li><Link href="/">Featured Events</Link></li>
             <li><Link href="/">Inventory</Link></li>
             <li><Link href="/">Docs</Link></li>
-          </ul> */}
+          </ul>
         </div>
         <div id="host">
-          <button className="btn-nav bg-gradient-to-r from-purple-500 to-purple-800 shadow-md text-white font-bold py-2 px-8 rounded-full">
-          {/* <h1  className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter"                                     data-aos="zoom-y-out"
->
-            
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-blue-400 "
-                  >
-            Coming Soon
-
-                            </span>
-
-          </h1>
-          
-          */}
+          <button className="btn-nav z-0 relative bg-gradient-to-r from-purple-500 to-purple-800 shadow-md text-white font-bold py-2 px-8 rounded-full">
           Coming Soon
           <img src="/comingsoon.png" className="img-nav inline ml-2"alt="" />
             </button>
         </div>
 
-        {/* {Toggle ? <img src="/hamburger (1).png" alt="" className="md:hidden h-[15px]" onClick={handleClick} /> : <img src="/close.png" alt="" className="md:hidden h-[15px]" onClick={handleClick}/>} */}
-
         <div id="Connect">
           {/* <button className="btn-hc">Connect Wallet</button> */}
         </div>
+
+        {Toggle ? <img src="/hamburger (1).png" alt="" className="z-0 md:hidden h-[20px]" onClick={handleClick} /> : <img src="/close.png" alt="" className="z-0 md:hidden h-[20px]" onClick={handleClick}/>}
+
       </nav>
     </div>
   );
