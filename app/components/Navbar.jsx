2bcb247fff6ac26589e94b64c2b8b14493bc3b00
unknown <yugankbansal.00001276033.aesl@gmail.com>
2023-09-17 18:36:03 +0530

    hamburger done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -15,20 +15,18 @@ export default function Navbar() {
       <nav id="navbar">
         <div></div>
        <Link href="/"> <img src="/logo.png" alt="logo" className="logo" /></Link>
-        <div className="items" id="features">
-
           <ul className="first hidden md:flex">
             <li><Link href="/">Featured Events</Link></li>
             <li><Link href="/">Inventory</Link></li>
             <li><Link href="/">Docs</Link></li>
           </ul>
-
-          <ul className="first md:hidden h-screen w-full fixed z-negative left-[0] top-[-80px]" >
-            <li><Link href="/">Featured Events</Link></li>
-            <li><Link href="/">Inventory</Link></li>
-            <li><Link href="/">Docs</Link></li>
+          
+          <ul className={ `delay-300 md:hidden h-screen bg-black/90 w-full fixed top-[80px] text-white text-left  flex-col ${Toggle? 'right-[100%]':'left-[100%]}'}`} >
+            <li className="p-5 md:p-[0px]"><Link href="/">Featured Events</Link></li>
+            <li className="p-5 md:p-[0px]"><Link href="/">Inventory</Link></li>
+            <li className="p-5 md:p-[0px]"><Link href="/">Docs</Link></li>
           </ul>
-        </div>
+          
         <div id="host">
           <button className="btn-nav z-0 relative bg-gradient-to-r from-purple-500 to-purple-800 shadow-md text-white font-bold py-2 px-8 rounded-full">
           Coming Soon
